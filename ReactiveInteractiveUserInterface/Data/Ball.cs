adfd0a56049f5d518301747c3111fe293b12f66f
zoravv <hadreq@gmail.com>
2026-04-16 12:16:41 +0200

    update .gitignore

---
+++ ReactiveInteractiveUserInterface/Data/Ball.cs
@@ -0,0 +1,52 @@
+﻿//____________________________________________________________________________________________________________________________________
+//
+//  Copyright (C) 2024, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community by pressing the `Watch` button and get started commenting using the discussion panel at
+//
+//  https://github.com/mpostol/TP/discussions/182
+//
+//_____________________________________________________________________________________________________________________________________
+
+namespace TP.ConcurrentProgramming.Data
+{
+    internal class Ball : IBall
+    {
+        #region ctor
+
+        internal Ball(Vector initialPosition, Vector initialVelocity)
+        {
+            _position = initialPosition;
+            Velocity = initialVelocity;
+        }
+
+        #endregion ctor
+
+        #region IBall
+
+        public event EventHandler<IVector>? NewPositionNotification;
+
+        public IVector Velocity { get; set; }
+
+        public IVector Position => _position;
+
+        #endregion IBall
+
+        #region private
+
+        private Vector _position;
+
+        private void RaiseNewPositionChangeNotification()
+        {
+            NewPositionNotification?.Invoke(this, Position);
+        }
+
+        internal void Move(Vector delta)
+        {
+            _position = new Vector(_position.x + delta.x, _position.y + delta.y);
+            RaiseNewPositionChangeNotification();
+        }
+
+        #endregion private
+    }
+}
